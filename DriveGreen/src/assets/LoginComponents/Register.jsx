521e88bcf65d770a9d91b28fb577ccb7ef552145
Vladimir-Tasic <tasicvlada987@gmail.com>
2025-05-06 19:29:42 +0200

    Register user UI

--- DriveGreen/src/assets/LoginComponents/Register.jsx
+++ DriveGreen/src/assets/LoginComponents/Register.jsx
@@ -1,3 +1,5 @@
+import arrowBack from '../Images/ArrowBack.png';
+import { Link } from 'react-router-dom';
 
 export function Register() {
 
@@ -5,7 +7,53 @@ return (
 
 <div id="register-card">
 
-            
+   <div className='register-header'>
+        <img src={arrowBack} alt="arroe-back" />
+        <Link 
+        to="/"
+        className='link-to-wellcome'
+        >Nazad</Link>
+    </div>  
+
+    <div className='register-title'>
+        <h1>Registruj se</h1>
+        <h4>Kreiranje naloga</h4>
+    </div>       
+
+    <div className='name-input'>
+        <label htmlFor="">
+            Ime i prezime
+            <br />
+            <input 
+            type="text"
+            placeholder='Upisite Vase ime i prezime...'
+            />
+        </label>
+    </div>
+
+    <div className='email-input'>
+        <label htmlFor="">
+            Email
+            <br />
+            <input 
+            type="text"
+            placeholder='Upisite Vas email...'
+            />
+        </label>
+    </div>
+
+    <div className='password-input'>
+        <label htmlFor="">
+            Lozinka
+            <br />
+            <input 
+            type="text"
+            placeholder='Upisite Vasu lozinku...'
+            />
+        </label>
+    </div>
+
+    <button>Registruj se</button>
 
 </div>
     )
