a29a0785bcbf89da1ddd0c1e31d9d95f4d251c30
Vladimir-Tasic <tasicvlada987@gmail.com>
2025-05-16 11:42:58 +0200

    Map Background image UI

--- DriveGreen/src/assets/MapComponents/MapStart.jsx
+++ DriveGreen/src/assets/MapComponents/MapStart.jsx
@@ -1,6 +1,7 @@
 
 import { MapFooter } from "../Layout/MapFooter"
 import { MapHeader } from "../Layout/MapHeader"
+import mapBackground from "../Images/MapBackground.png"
 
 export function MapStart() {
 
@@ -11,7 +12,10 @@ export function MapStart() {
         <div id="map-start-container">
 
             <MapHeader/>
-                
+                <img 
+                className="map-background"
+                src={mapBackground} 
+                alt="background" />
             <MapFooter/>
             
         </div>
