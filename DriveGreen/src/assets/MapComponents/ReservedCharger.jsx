6165add04c0fc3ef1bc3d6e2f135718342413413
Filip-art1 <jasam123filip@gmail.com>
2025-05-29 13:36:36 +0200

    reserved charger

--- DriveGreen/src/assets/MapComponents/ReservedCharger.jsx
+++ DriveGreen/src/assets/MapComponents/ReservedCharger.jsx
@@ -34,60 +34,64 @@ export function ReservedCharger() {
     };
 
     return (
-        !showCard ? (<div id="reserved-container">
-            <MapHeader />
+        !showCard ? (
+            <div id="reserved-container">
+                <MapHeader />
 
-            <div style={{ display: visible ? 'block' : 'none' }}>
-                <img src={ReservedIcon} alt="icon reserved" className="icon" />
-                <img src={CoungratulationBackground} alt="background image" className="background" />
-                <div className="top-div">
-                    <h1>Čestitamo!</h1>
-                    <p>Vaš punjač je rezervisan!</p>
+                <div style={{ display: visible ? 'block' : 'none' }}>
+                    <img src={ReservedIcon} alt="icon reserved" className="icon" />
+                    <img src={CoungratulationBackground} alt="background image" className="background" />
+                    <div className="top-div">
+                        <h1>Čestitamo!</h1>
+                        <p>Vaš punjač je rezervisan!</p>
+                    </div>
+
+                    <img src={DottedLine} alt="dotted line" className="dotted-line" />
+
+                    <div className="middle-div">
+                        <div className="middle-first">
+                            <h1>Stanica</h1>
+                            <p>Robert Bosch Charging</p>
+                        </div>
+
+                        <div className="middle-second">
+                            <h1>Dolazak</h1>
+                            <p>31. Maj, 2025, 09:45 am</p>
+                        </div>
+
+                        <div className="middle-third">
+                            <h1>Trajanje</h1>
+                            <p>1hr 30min</p>
+                        </div>
+
+                        <div className="middle-fourth">
+                            <h1>Ukupno</h1>
+                            <p>Besplatno</p>
+                        </div>
+                    </div>
+
+                    <div className="buttons-div">
+                        <button
+                            onClick={() => {
+                                handleDrawRoute();
+                                showCardAfterSeconds();
+                            }}
+                            className="button-one"
+                        >
+                            prikaži rutu
+                        </button>
+
+                        <Link className="button-two-link" to="/viewMore">
+                            otkaži rezervaciju
+                        </Link>
+                    </div>
                 </div>
 
-                <img src={DottedLine} alt="dotted line" className="dotted-line" />
-
-                <div className="middle-div">
-                    <div className="middle-first">
-                        <h1>Stanica</h1>
-                        <p>Robert Bosch Charging</p>
-                    </div>
-
-                    <div className="middle-second">
-                        <h1>Dolazak</h1>
-                        <p>31. Maj, 2025, 09:45 am</p>
-                    </div>
-
-                    <div className="middle-third">
-                        <h1>Trajanje</h1>
-                        <p>1hr 30min</p>
-                    </div>
-
-                    <div className="middle-fourth">
-                        <h1>Ukupno</h1>
-                        <p>Besplatno</p>
-                    </div>
-                </div>
-
-                <div className="buttons-div">
-                    <button
-                        onClick={() => {
-                            handleDrawRoute();
-                            showCardAfterSeconds();
-                        }}
-                        className="button-one"
-                    >prikaži rutu</button>
-
-                    <Link
-                        className="button-two-link"
-                        to="/viewMore"
-                    >otkaži rezervaciju</Link>
-                </button>
+                <MapBackground ref={mapRef} style={{ display: visible ? 'none' : 'block' }} />
+                <MapFooter />
             </div>
-            <MapBackground ref={mapRef} style={{ display: visible ? 'none' : 'block' }} />
-            <MapFooter />
-        </div>) : (<SuccessfulDestination />)
-
-    )
-
+        ) : (
+            <SuccessfulDestination />
+        )
+    );
 }
